d627e31d9b62148cca495767959e31e1569acf70
dasep12 <depiyawandasep13@gmail.com>
2022-03-30 15:40:57 +0700

    logo

--- screens/Logout.js
+++ screens/Logout.js
@@ -7,7 +7,6 @@ export default function  Logout ({navigation,route}) {
     const [loading,setLoading] = useState(true);
 
     const  logout = async() => {
-      try {
         await AsyncStorage.removeItem('id_token');
         await AsyncStorage.removeItem("token");
         await AsyncStorage.removeItem("id_akun");
@@ -15,20 +14,16 @@ export default function  Logout ({navigation,route}) {
         await AsyncStorage.removeItem("token_patrol");
         await AsyncStorage.removeItem("token_device");
         navigation.navigate('Login');
-        return true;
-      }
-      catch(exception) {
-        return false;
-      }
     }
   useEffect(() => {
-    //logout();
+    logout();
   }, []);
   
 
   
     return (
-        <View style={{flex : 1 , justifyContent:'center' }}>
+        <View style={{alignItems: "center",
+        justifyContent: "center" , flex:1 }}>
            <Text>BYE</Text>
         </View>
     );
